3c38e28b47a2e7df867f136c1fa51a69aad256e6
jcst2019 <juansolar_4@hotmail.com>
2023-08-12 01:15:07 -0500

    fin seccion 9

--- models/usuario.js
+++ models/usuario.js
@@ -36,7 +36,7 @@ const UsuarioSchema = Schema({
 
 
 UsuarioSchema.methods.toJSON = function() {
-    const { __v, password, ...usuario  } = this.toObject();
+    const { __v, password, ...usuario  } = this.toObject(); // Se omite __v y password y los demas campos se almacenan en usuario que lo retorno al final
     return usuario;
 }
 //Usuario en singular porque Mongose le coloca Usuarios el nombre de la coleccion(Le agrega la s)
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,17 +6,40 @@ const { usuariosGet,
         usuariosPost,
         usuariosDelete,
         usuariosPatch } = require('../controllers/usuarios');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const router = Router();
 
 
 router.get('/', usuariosGet );
 
-router.put('/:id', usuariosPut );
+//router.put('/:id', usuariosPut );
 
-router.post('/', usuariosPost );
+router.put('/:id',[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    check('rol').custom( esRoleValido ), 
+    validarCampos
+],usuariosPut );
 
-router.delete('/', usuariosDelete );
+//router.post('/', usuariosPost );
+router.post('/',[
+check('nombre','El Nombre es Obligatorio').not().isEmpty(),
+check('password','El password es Obligatorio y más de 6 caracteres').isLength({min:6}),
+check('correo','El correo No es valido').isEmail(),
+//check('rol','No es un rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+check('rol').custom(esRoleValido), // Es equivalente a (rol =>esRoleValido(rol) )
+check('correo').custom(emailExiste), // Es equivalente a (rol =>esRoleValido(rol) )
+validarCampos
+], usuariosPost );
+
+router.delete('/:id',[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    validarCampos
+],usuariosDelete );
 
 router.patch('/', usuariosPatch );
 
